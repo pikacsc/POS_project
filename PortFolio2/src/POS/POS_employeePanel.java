1b0cc4c39f21563d021c83344af687a4f74be358
JasonChoi <ccc11203@gmail.com>
2018-01-29 18:27:20 +0900

    소스 리팩토링, POS_employeePanel,goodsPanel 분리

---
+++ PortFolio2/src/POS/POS_employeePanel.java
@@ -0,0 +1,349 @@
+package POS;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+
+class POS_employeePanel extends JPanel {
+	String userLevel;
+	String userid;
+	String selectedLevel;
+	String selectedId;
+	DAO dao;
+	employeeDialog eDialog;
+	
+	DefaultTableModel eModel;
+	JTable eTable;
+
+	JPanel center;// 테이블
+	JPanel east;// 버튼들
+	
+
+	JButton selectBtn;// 직원조회
+	JButton insertBtn;// 입력
+	JButton updateBtn;// 수정
+	JButton deleteBtn;// 삭제
+	JButton searchBtn;// 검색
+	JButton logoutBtn;// 로그아웃
+	
+	
+	public POS_employeePanel(String userLevel,String userid,DAO dao) {
+		this.dao = dao;
+		this.userLevel = userLevel;
+		this.userid = userid;
+		eDialog = new employeeDialog("직원등록",userLevel);
+		
+		String colName[] = { "ID","이름","직급","입사일자" };
+		eModel = new DefaultTableModel(colName, 0);
+		layoutSetting();
+		listenerSetting();
+	}
+	
+	void listenerSetting() {
+		eTable.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				eTable = (JTable) e.getComponent();
+				eModel = (DefaultTableModel) eTable.getModel();
+
+				// 테이블에서 선택시 ID를 가져와서 삭제할때나 수정할때 쓸수있도록함
+				selectedId = (String) eModel.getValueAt(eTable.getSelectedRow(), 0);
+				selectedLevel = (String) eModel.getValueAt(eTable.getSelectedRow(), 2);
+				
+				System.out.println(selectedId);
+			
+			}
+		});
+		
+		
+		
+		selectBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dao.selectEmployee(eModel);
+			}
+		});
+		
+		if(userLevel.equals("직원") == false) {
+			insertBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+						eDialog.setVisible(true);
+				}
+			});
+			deleteBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if(userid.equals(selectedId)) {
+						JOptionPane.showMessageDialog(null, "자기 자신은 삭제할수 없습니다.", "Error", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+					else if (userLevel.equals("사장")) {
+						deleteProcess();
+					}
+					else {
+						if(selectedLevel.equals("사장")||selectedLevel.equals("점장")) {
+							JOptionPane.showMessageDialog(null, "권한이 없습니다.", "Error", JOptionPane.ERROR_MESSAGE);
+							return;
+						}else {
+							deleteProcess();
+						}
+					}
+				
+				}
+			});
+		}
+		
+		updateBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(selectedId.equals(userid)) {
+					eDialog.setVisible(true);
+				}else if(userid.equals("직원")) {
+					JOptionPane.showMessageDialog(null, "권한이 없습니다.", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}else {
+					eDialog.setVisible(true);
+				}
+			}
+		});
+		
+		logoutBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+	}
+	
+	void deleteProcess() {
+		if (JOptionPane.showConfirmDialog(null, "삭제하시겠습니까?", "삭제",
+				JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+			try {
+				String result = dao.deleteEmployee(selectedId);
+				if (result.equals("삭제되었습니다.")) {
+					JOptionPane.showMessageDialog(null, "삭제 되었습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
+					dao.selectEmployee(eModel);
+				} else {
+					JOptionPane.showMessageDialog(null, "삭제 실패" + result, "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			} catch (Exception a) {
+				JOptionPane.showMessageDialog(null, "오류"+a.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+		} else {
+			return;
+		}
+	}
+	
+	
+	void layoutSetting(){
+		setLayout(new BorderLayout());
+		center = new JPanel();
+		east = new JPanel();
+		
+
+		center.setLayout(new FlowLayout());
+		east.setLayout(new FlowLayout());
+		eTable = new JTable(eModel);
+		eTable.setPreferredScrollableViewportSize(new Dimension(500, 300));
+
+		center.add(new JScrollPane(eTable));
+		selectBtn = new JButton("직원 목록 조회");
+		if(userLevel.equals("직원")==false) {
+			insertBtn = new JButton("직원 등록");
+			deleteBtn = new JButton("직원 정보 삭제");
+			east.add(insertBtn);
+			east.add(deleteBtn);
+		}
+		updateBtn = new JButton("직원 정보 수정");
+		logoutBtn = new JButton("로그아웃");
+		east.add(selectBtn);
+		east.add(updateBtn);
+		east.add(logoutBtn);
+		add(center, BorderLayout.CENTER);
+		add(east, BorderLayout.SOUTH);
+		setSize(280, 400);
+	}
+	
+	
+	class employeeDialog extends JDialog {
+		JLabel nameLabel;
+		JLabel idLabel;
+		JLabel pwLabel;
+		JLabel levelLabel;
+		String userLevel;
+		
+		JTextField idTf;
+		JPasswordField pwTf;
+		JTextField nameTf;
+		JTextField levelTf;
+		
+		JButton confirmBtn;
+		JButton resetBtn;
+		
+		JPanel centerPanel;
+		JPanel southPanel;
+
+		public employeeDialog(String title,String userLevel) {
+			this.userLevel = userLevel;
+			
+			setTitle(title);
+			layoutSetting();
+			listenerSetting();
+
+		}
+		
+		void layoutSetting() {
+			setLayout(new BorderLayout());
+			
+			nameLabel = new JLabel("이름");
+			nameLabel.setBounds(20,10,80,25);
+			
+			
+			idLabel = new JLabel("ID");
+			idLabel.setBounds(20,40,80,25);
+			
+			pwLabel = new JLabel("pw");
+			pwLabel.setBounds(20, 70, 80, 25);
+			
+			nameTf = new JTextField(10);
+			nameTf.setBounds(50, 10, 160, 25);
+			
+			
+			idTf = new JTextField(10);
+			idTf.setBounds(50, 40, 160, 25);
+			
+			pwTf = new JPasswordField(10);
+			pwTf.setBounds(50, 70, 160, 25);
+			
+			levelLabel = new JLabel("직급");				
+			levelLabel.setBounds(20, 100, 80, 25);
+			
+			levelTf = new JTextField(10);
+			levelTf.setBounds(50, 100, 160, 25);
+			levelTf.setVisible(false);
+			levelLabel.setVisible(false);
+
+			
+			
+			confirmBtn = new JButton("확인");
+			resetBtn = new JButton("초기화");
+				
+			
+			centerPanel = new JPanel();
+			southPanel = new JPanel();
+			
+			
+			
+			centerPanel.setLayout(null);
+			centerPanel.add(nameLabel);
+			centerPanel.add(nameTf);
+			centerPanel.add(idTf);
+			centerPanel.add(idLabel);
+			centerPanel.add(idTf);
+			centerPanel.add(pwLabel);
+			centerPanel.add(pwTf);
+			centerPanel.add(levelLabel);
+			centerPanel.add(levelTf);
+			
+			
+			southPanel.setLayout(new FlowLayout());
+			southPanel.add(confirmBtn);
+			southPanel.add(resetBtn);
+			
+			if(userLevel.equals("사장")) {
+				levelTf.setVisible(true);
+				levelLabel.setVisible(true);
+			}
+			
+			add(centerPanel, BorderLayout.CENTER);
+			add(southPanel, BorderLayout.SOUTH);
+			setSize(250, 450);
+		}
+		
+		void listenerSetting() {
+			confirmBtn.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if(userLevel.equals("사장")) {
+						String name = nameTf.getText();
+						String id = idTf.getText();
+						String pw = new String(pwTf.getPassword());
+						String level = levelTf.getText();
+						dao.m_insertEmployee(name, id, pw, level);
+					}else {
+						String name = nameTf.getText();
+						String id = idTf.getText();
+						String pw = new String(pwTf.getPassword());
+						dao.insertEmployee(name, id, pw);
+					}
+				}
+			});
+			
+			resetBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					nameTf.setText("");
+					idTf.setText("");
+					pwTf.setText("");
+					if(userLevel.equals("사장")) {
+						levelTf.setText("");
+					}
+				}
+			});
+			
+			
+			addWindowListener(new WindowAdapter() {
+
+				@Override
+				public void windowClosing(WindowEvent e) {
+					nameTf.setText("");
+					idTf.setText("");
+					pwTf.setText("");
+					if(userLevel.equals("사장")) {
+						levelTf.setText("");
+					}
+					setVisible(false);
+				}
+			});
+			
+		
+		}
+		
+		
+	}
+
+}
+
+
+
+
---
+++ PortFolio2/src/POS/POS_goodsPanel.java
@@ -0,0 +1,223 @@
+package POS;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+class POS_goodsPanel extends JPanel {
+	DAO dao;
+	DefaultTableModel model;
+	JTable table;
+
+	
+	JPanel north;// 텍스트필드
+	JPanel center;// 테이블
+	JPanel south;// 버튼들
+
+	JTextField gName;// 상품이름
+	JTextField gPrice;// 가격
+	JTextField gCount;// 재고
+	JTextField gCode;// 바코드
+	JButton selectBtn;// 목록조회
+	JButton insertBtn;// 입력
+	JButton updateBtn;// 수정
+	JButton deleteBtn;// 삭제
+	JButton searchBtn;// 검색
+
+	public POS_goodsPanel(DAO dao) {
+		this.dao = dao;
+		String colName[] = { "품명", "가격", "재고", "바코드" };
+		model = new DefaultTableModel(colName, 0);
+		layoutSetting();
+		listenerSetting();
+		
+	}
+
+	void listenerSetting() {
+		table.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				table = (JTable) e.getComponent();
+				model = (DefaultTableModel) table.getModel();
+
+				// 품명
+				String sgName = (String) model.getValueAt(table.getSelectedRow(), 0);
+				gName.setText(sgName);
+
+				// 가격
+				String sgPrice = (String) model.getValueAt(table.getSelectedRow(), 1);
+				gPrice.setText(sgPrice);
+
+				// 재고
+				String sgCount = (String) model.getValueAt(table.getSelectedRow(), 2);
+				gCount.setText(sgCount);
+
+				// 바코드
+				String sgCode = (String) model.getValueAt(table.getSelectedRow(), 3);
+				gCode.setText(sgCode);
+			}
+		});
+		
+		selectBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dao.selectGoods(model);
+			}
+		});
+		
+		insertBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if (gName.getText().equals("")) {
+						JOptionPane.showMessageDialog(null, "품명을 입력하세요");
+						gName.requestFocus(); // 필드로 커서가 가짐
+					} else if (gPrice.getText().equals("")) {
+						JOptionPane.showMessageDialog(null, "가격을 입력하세요");
+						gPrice.requestFocus(); // 필드로 커서가 가짐
+					} else if (gCount.getText().equals("")) {
+						JOptionPane.showMessageDialog(null, "재고를 입력하세요");
+						gCount.requestFocus(); // 필드로 커서가 가짐
+					} else if (gCode.getText().equals("")) {
+						JOptionPane.showMessageDialog(null, "바코드를 입력하세요");
+						gCode.requestFocus(); // 필드로 커서가 가짐
+					} else {
+						String name = gName.getText();
+						int price = Integer.parseInt(gPrice.getText());
+						int count = Integer.parseInt(gCount.getText());
+						String code = gCode.getText();
+						String result = dao.insertGoods(name, price, count, code);
+						if (result.equals("입력되었습니다.")) {
+							JOptionPane.showMessageDialog(null, "입력되었습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
+							dao.selectGoods(model);
+						} else if (result.substring(0, 9).equals("ORA-00001")) {
+							JOptionPane.showMessageDialog(null, "입력실패, 바코드가 중복됩니다", "Error",
+									JOptionPane.ERROR_MESSAGE);
+						} else {
+							JOptionPane.showMessageDialog(null, "입력실패" + result, "Error",
+									JOptionPane.ERROR_MESSAGE);
+						}
+					}
+				} catch (Exception a) {
+					JOptionPane.showMessageDialog(null, "숫자만 입력하세요", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+			}
+		});
+
+		updateBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (JOptionPane.showConfirmDialog(null, "수정하시겠습니까?", "수정",
+						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+					try {
+						String name = gName.getText();
+						int price = Integer.parseInt(gPrice.getText());
+						int count = Integer.parseInt(gCount.getText());
+						String code = gCode.getText();
+						String result = dao.updateGoods(name, price, count, code);
+						if (result.equals("수정되었습니다.")) {
+							JOptionPane.showMessageDialog(null, "수정 되었습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
+							dao.selectGoods(model);
+						} else {
+							JOptionPane.showMessageDialog(null, "수정 실패" + result, "Error",
+									JOptionPane.ERROR_MESSAGE);
+						}
+					} catch (Exception a) {
+						JOptionPane.showMessageDialog(null, "숫자만 입력하세요", "Error", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				} else {
+					return;
+				}
+			}
+		});
+
+		deleteBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Yes 버튼을 눌렀을때
+				if (JOptionPane.showConfirmDialog(null, "삭제하시겠습니까?", "삭제",
+						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+					try {
+						String code = gCode.getText();
+						String result = dao.deleteGoods(code);
+						if (result.equals("삭제되었습니다.")) {
+							JOptionPane.showMessageDialog(null, "삭제 되었습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
+							dao.selectGoods(model);
+						} else {
+							JOptionPane.showMessageDialog(null, "삭제 실패" + result, "Error",
+									JOptionPane.ERROR_MESSAGE);
+						}
+					} catch (Exception a) {
+						JOptionPane.showMessageDialog(null, "숫자만 입력하세요", "Error", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				} else {
+					return;
+				}
+			}
+		});
+	}
+
+	void layoutSetting() {
+		setLayout(new BorderLayout());
+		north = new JPanel();
+		center = new JPanel();
+		south = new JPanel();
+
+		gName = new JTextField(20);
+		gPrice = new JTextField(20);
+		gCount = new JTextField(20);
+		gCode = new JTextField(20);
+
+		north.setLayout(new FlowLayout());
+		center.setLayout(new FlowLayout());
+		south.setLayout(new FlowLayout());
+		north.add(new JLabel("상품이름:"));
+		north.add(gName);
+		north.add(new JLabel("가격:"));
+		north.add(gPrice);
+		north.add(new JLabel("재고:"));
+		north.add(gCount);
+		north.add(new JLabel("바코드:"));
+		north.add(gCode);
+		table = new JTable(model);
+		table.setPreferredScrollableViewportSize(new Dimension(500, 300));
+
+		center.add(new JScrollPane(table));
+		selectBtn = new JButton("목록조회");
+		insertBtn = new JButton("입력");
+		updateBtn = new JButton("수정");
+		deleteBtn = new JButton("삭제");
+
+		south.add(selectBtn);
+		south.add(insertBtn);
+		south.add(updateBtn);
+		south.add(deleteBtn);
+		add(north, BorderLayout.NORTH);
+		add(center, BorderLayout.CENTER);
+		add(south, BorderLayout.SOUTH);
+		setSize(280, 400);
+
+	}
+
+}
