037297a6fd2c3791293b4c642f26caf936a9e844
JasonChoi <ccc11203@gmail.com>
2018-01-31 17:27:56 +0900

    계산 패널 구현 시작

---
+++ PortFolio2/src/POS/POS_calculator.java
@@ -0,0 +1,22 @@
+package POS;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class POS_calculator extends JPanel{
+	JPanel centerPanel;
+	JPanel eastPanel;
+	JPanel southPanel;
+	JPanel northPanel;
+	public POS_calculator() {
+		setLayout(new BorderLayout());
+	}
+	
+	void layoutSetting() {
+		
+	}
+	void listenerSetting() {
+		
+	}
+}
